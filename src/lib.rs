a5cbc53b8eefd9f4b32e8d2179cbe2def141f3e6
Dhruv kumar <darbaga10@gmail.com>
2017-02-24 19:25:03 +0530

    Add a function and a test

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,16 @@
 #[link(name = "tolk")]
 extern {
     fn Tolk_Load();
+    fn Tolk_IsLoaded() -> bool;
     fn Tolk_Unload();
 }
 
+#[test]
+fn load_unload() {
+    unsafe {
+        Tolk_Load();
+        assert_eq!(Tolk_IsLoaded(), true);
+        Tolk_Unload();
+        assert_eq!(Tolk_IsLoaded(), false);
+    }
+}
